15c6efbe96ec4fdf88dcbbb7af8b2deb6f0cb79a
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-09-27 18:09:19 -0300

    Estilizando

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -4,7 +4,11 @@ import logo from '../../imagens/logo.svg'
 export default function Logo(){
     return(
         <div className="logo">
-            <img src={logo} alt="logo"/>
+            <img
+                src={logo}
+                alt="logo"
+                className='logo-img'
+            />
             <p><b>Alura</b> Books</p>
         </div>
     )
