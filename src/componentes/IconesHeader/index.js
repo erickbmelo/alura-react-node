4582648cd197c0a5a7ef7ef052f81d8a9a04b2eb
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-09-29 12:23:53 -0300

    Trocando o restante dos arquivos .CSS por styled-components

--- src/componentes/IconesHeader/index.js
+++ src/componentes/IconesHeader/index.js
@@ -1,15 +1,25 @@
-import './estilo.css'
 import perfil from '../../imagens/perfil.svg'
 import sacola from '../../imagens/sacola.svg'
+import styled from 'styled-components'
+
+const Icone = styled.li`
+    margin-right: 40px;
+    width: 25px;
+`
+
+const Icones = styled.ul`
+    display: flex;
+    align-items: center;
+`
 
 const icones = [perfil, sacola]
 
 export default function IconesHeader() {
     return (
-        <ul className='icones'>
+        <Icones>
             { icones.map((icone) => (
-                <li className='icone'><img src={icone} alt="" /></li>
+                <Icone><img src={icone} alt="" /></Icone>
             ))}
-        </ul>
+        </Icones>
     )
 }
--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,15 +1,23 @@
-import './estilo.css'
+import styled from 'styled-components'
 import logo from '../../imagens/logo.svg'
+
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px;
+`
+
+const LogoImage = styled.img`
+    margin-right: 10px;
+`
 
 export default function Logo(){
     return(
-        <div className="logo">
-            <img
+        <LogoContainer>
+            <LogoImage
                 src={logo}
                 alt="logo"
-                className='logo-img'
             />
             <p><b>Alura</b>Books</p>
-        </div>
+        </LogoContainer>
     )
 }
