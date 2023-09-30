8b9fb0d86852547abed2572b13589b80a8fd9112
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-09-30 20:47:55 -0300

    Usando States

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,35 +1,40 @@
 import Input from "../Input"
 import styled from "styled-components"
+import { useState } from "react"
 
 const PesquisaContainer = styled.section`
-        color: #FFF;
-        text-align: center;
-        padding: 85px 0;
-        height: 270px;
-        width: 100%;
+    color: #FFF;
+    text-align: center;
+    padding: 85px 0;
+    height: 270px;
+    width: 100%;
 `
 
 const Titulo = styled.h2`
-        color: #FFF;
-        font-size: 36px;
-        text-align: center;
-        width: 100%;
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
 `
 
 const Subtitulo = styled.h3`
-        font-size: 16px;
-        font-weight: 500;
-        margin-bottom: 40px;
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 40px;
 `
 
 export default function Pesquisa (){
+    const [textoDigitado, setTextoDigitado] = useState('')
+                                       
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
             <Input
                 placeholder="Escreva sua próxima leitura"
-                />
+                onBlur={evento => setTextoDigitado(evento.target.value)}
+            />
+            <p>{textoDigitado}</p>
         </PesquisaContainer>
     )
 }
