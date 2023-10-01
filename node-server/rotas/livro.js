f05edec1b1cd2dcc25840a4ed54311eefb5e0e28
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-10-01 18:29:00 -0300

    Status HTTP

--- node-server/rotas/livro.js
+++ node-server/rotas/livro.js
@@ -3,7 +3,14 @@ const { Router } = require("express")
 const router = Router()
 
 router.get("/", (req, res) => {
-    res.send("<h1>Olá, mundo!</h1>")
+
+    try {
+        res.send("<h1>Olá, mundo!</h1>")
+    }
+    catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
 })
 
 router.post("/", (req, res) => {
