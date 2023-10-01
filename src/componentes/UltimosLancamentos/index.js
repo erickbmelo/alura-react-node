d21996918a8c52058cbf18dc38f0662b4103201e
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-09-30 22:07:06 -0300

    Card reutilizável com props

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,5 +1,7 @@
 import { livros } from './dadosUltimosLancamentos'
 import { Titulo } from '../Titulo'
+import CardRecomenda from '../CardRecomenda'
+import imagemLivro from '../../imagens/livro2.png'
 import styled from 'styled-components'
 
 const UltimosLancamentosContainer = styled.section`
@@ -21,8 +23,9 @@ function UltimosLancamentos() {
     return (
         <UltimosLancamentosContainer>
             <Titulo
-                cor="#EB9B00"
-                tamanhoFonte="36px">
+                cor={"#EB9B00"}
+                tamanhoFonte={"36px"}
+            >
                 ÚLTIMOS LANÇAMENTOS
             </Titulo>
             <NovosLivrosContainer>
@@ -30,6 +33,12 @@ function UltimosLancamentos() {
                     <img src={livro.src} alt=""/>
                 ) ) }
             </NovosLivrosContainer>
+            <CardRecomenda
+                titulo="Talvez você se interesse por"
+                subtitulo="Angular 11"
+                descricao="Construindo uma aplicação com a plataforma Google"
+                img={imagemLivro}
+            />
         </UltimosLancamentosContainer>
     )
 }
