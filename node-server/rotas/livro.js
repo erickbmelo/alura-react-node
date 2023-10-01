59e4b26876c823a8bb431231944d378ffd9a032f
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-10-01 18:20:38 -0300

    Rotas de post, patch e delete

--- node-server/rotas/livro.js
+++ node-server/rotas/livro.js
@@ -6,4 +6,16 @@ router.get("/", (req, res) => {
     res.send("<h1>Olá, mundo!</h1>")
 })
 
+router.post("/", (req, res) => {
+    res.send("Você fez uma requisição do tipo POST")
+})
+
+router.patch("/", (req, res) => {
+    res.send("Você fez uma requisição do tipo patch")
+})
+
+router.delete("/", (req, res) => {
+    res.send("Você fez uma requisição do tipo delete")
+})
+
 module.exports = router
